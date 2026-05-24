d8d79cf5dbe67db92c4a58e00d6ff00fcd61b0fb
Anton Maminov <anton.linux@gmail.com>
2026-05-24 21:44:17 +0300

    add specs for QTranslator

--- spec/core_spec.cr
+++ spec/core_spec.cr
@@ -486,6 +486,22 @@ describe Qt6 do
     application.window_icon = previous_window_icon
   end
 
+  it "supports application translators" do
+    app
+    fixture_dir = File.join(__DIR__, "fixtures")
+    translator = Qt6::QTranslator.new
+
+    Qt6.translate("TranslationSpec", "Hello, world!").should eq("Hello, world!")
+    translator.load("translation_spec_uk.qm", fixture_dir).should be_true
+    Qt6.install_translator(translator).should be_true
+    Qt6.translate("TranslationSpec", "Hello, world!").should eq("Привіт, світе!")
+    Qt6.translate("TranslationSpec", "Missing translation").should eq("Missing translation")
+    Qt6.remove_translator(translator).should be_true
+    Qt6.translate("TranslationSpec", "Hello, world!").should eq("Hello, world!")
+
+    translator.release
+  end
+
   it "supports theme-aware palettes on applications and widgets" do
     application = app
     previous_palette = application.palette
---
+++ spec/fixtures/translation_spec_uk.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk_UA">
+  <context>
+    <name>TranslationSpec</name>
+    <message>
+      <source>Hello, world!</source>
+      <translation>Привіт, світе!</translation>
+    </message>
+  </context>
+</TS>
